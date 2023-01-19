225efcb944a890e6561aee771529afefa0a0d08e
Jhonny Camacho E <makykamacho@gmail.com>
2023-01-19 19:33:06 -0400

    Implement Education Page

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Portrait from '../../components/home/Portrait';
+import Timeline from '../../components/education/Timeline';
 
 const EducationPage = () => {
   return (
-    <section>
-      <div className="flex justify-between gap-12 sm:gap-52 mt-8 mr-14 flex-wrap">
-        <span>Education Page</span>
+    <section className=" w-full flex justify-between mt-8 flex-wrap">
+      <div className="w-4/6">
+        <Timeline />
+      </div>
+      <div className="w-2/6">
         <Portrait />
       </div>
     </section>
---
+++ components/education/listOfCourses.js
@@ -0,0 +1,22 @@
+const listOfCourses = [
+  {
+    year: 2017,
+    title: 'Bachelor of humanities',
+    description: 'Colegio Don Bosco',
+    source: ''
+  },
+  {
+    year: 2022,
+    title: 'Bachelor of Systems Engineering',
+    description: 'Institution: Universidad Católica Boliviana',
+    source: ''
+  },
+  {
+    year: 2022,
+    title: 'Fullstack Developer',
+    description: 'Institution: Core Code Bootcamp',
+    source: 'https://api.badgr.io/public/assertions/UwHfpWLiSXOXo6zYs1MyoQ?identity__email=makykamacho%40gmail.com'
+  }
+];
+
+export default listOfCourses;
